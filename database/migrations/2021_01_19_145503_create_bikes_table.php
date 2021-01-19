66c3d0b3125a266fdd1be0b36d414b109eb539dc
FabioMezzina <fabio.mezzina.09@gmail.com>
2021-01-19 15:59:42 +0100

    create bikes migration

---
+++ database/migrations/2021_01_19_145503_create_bikes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBikesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bikes', function (Blueprint $table) {
+            $table->id();
+            $table->string('brands', 30);
+            $table->string('model', 30);
+            $table->string('power', 10);
+            $table->string('color', 30);
+            $table->smallInteger('price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bikes');
+    }
+}
